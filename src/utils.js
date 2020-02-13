8dccee6a82c02287110c7b57fd62388d4e2d84a7
Kevin Connor <kevinpaulconnor@gmail.com>
2020-02-13 08:31:34 -0600

    handle non-number student input, and sample scenarios

--- src/utils.js
+++ src/utils.js
@@ -11,16 +11,97 @@ export const RESULTS = {
     "INVALID": "INVALID"
 }
 
-export function convertTemperature(inputUnit, input, targetUnit, target) {
-    if (arguments.length !== 3) {
+// we know we have rounded the inputs to ones, and know that the
+// conversion formulas go to two decimal digits at most.
+// therefore we should be safe to multiply without running
+// into floating point precision errors,
+// as these constants will be the only floats
+const fiveNinths = .55;
+const nineFifths = 1.8;
+const conversionFxns = {
+    "CELSIUS": {
+        "FAHRENHEIT": (value) =>{
+            return (nineFifths*value) + 3200
+        },
+        "KELVIN": (value) => {
+            return value + 27315
+        },
+        "RANKINE": (value) => {
+            return (value + 27315) * nineFifths
+        }
+    },
+    "FAHRENHEIT": {
+        "CELSIUS": (value) =>{
+            return (value-3200) * fiveNinths
+        },
+        "KELVIN": (value) => {
+            return (value + 45967) * fiveNinths
+        },
+        "RANKINE": (value) => {
+            return (value + 45967)
+        }
+    },
+    "KELVIN": {
+        "FAHRENHEIT": (value) =>{
+            return (value * nineFifths) - 45967
+        },
+        "CELSIUS": (value) => {
+            return value - 27315
+        },
+        "RANKINE": (value) => {
+            return value * nineFifths
+        }
+    },
+    "RANKINE": {
+        "FAHRENHEIT": (value) =>{
+            return value - 45967
+        },
+        "CELSIUS": (value) => {
+            return (value - 49167) * fiveNinths
+        },
+        "KELVIN": (value) => {
+            return value * fiveNinths
+        }
+    },
+    
+}
+
+function calculateConversion(inputUnit, input, targetUnit, roundedTarget) {
+    let result = conversionFxns[inputUnit][targetUnit](input*100);
+    result = roundToOnes(result / 100);
+    if (((result - 1) <= roundedTarget) &&
+        (roundedTarget <= (result + 1))) {
+        return RESULTS.CORRECT;
+    }
+    return RESULTS.INCORRECT;
+}
+
+function roundToOnes(value) {
+    let result = value.toFixed(0);
+    // per the sample results in the spec, .5 should round down,
+    // but toFixed rounds it up
+    // so we need a special case
+    let splitString = value.toString().split('.');
+    if ((splitString[1] - 5) === 0) {
+        result--;
+    }
+    return Number(result);
+}
+
+export function convertTemperature(inputUnit, input, targetUnit, target) {   
+    if (arguments.length !== 4) {
         return RESULTS.INVALID;
     }
     if (UNITS[inputUnit] === undefined || UNITS[targetUnit] === undefined) {
         return RESULTS.INVALID;
     }
-    if (typeof(input) !== "number" || typeof(target) !== "number") {
-        return RESULTS.INVALID
+    if (typeof(input) !== "number") {
+        return RESULTS.INVALID;
+    }
+    if (typeof(target) !== "number") {
+        return RESULTS.INCORRECT;
     }
 
-
+    let roundedTarget = roundToOnes(target);
+    return calculateConversion(inputUnit, input, targetUnit, roundedTarget);
 }
--- src/utils.test.js
+++ src/utils.test.js
@@ -13,5 +13,17 @@ it('does not accept invalid units', () => {
     expect(convertTemperature('badUnit', 1, UNITS.CELSIUS, 2)).toEqual(RESULTS.INVALID);
     expect(convertTemperature(UNITS.CELSIUS, 1, 'badUnit', 2)).toEqual(RESULTS.INVALID);
     expect(convertTemperature(UNITS.CELSIUS, "1", UNITS.KELVIN, 2)).toEqual(RESULTS.INVALID);
-    expect(convertTemperature(UNITS.CELSIUS, 1, UNITS.KELVIN, "2")).toEqual(RESULTS.INVALID);
 });
+
+it('marks non-number student input as incorrect', () => {
+    expect(convertTemperature(UNITS.CELSIUS, 1, UNITS.KELVIN, "cat")).toEqual(RESULTS.INCORRECT);
+});
+
+it ('handles the example scenarios', () => {
+    expect(convertTemperature(UNITS.FAHRENHEIT, 84.2, UNITS.RANKINE, 543.5)).toEqual(RESULTS.CORRECT);
+    expect(convertTemperature(UNITS.CELSIUS, -45.14, UNITS.KELVIN, 227.51)).toEqual(RESULTS.CORRECT);
+    expect(convertTemperature(UNITS.KELVIN, 317.33, UNITS.FAHRENHEIT, 110.5)).toEqual(RESULTS.INCORRECT);
+    expect(convertTemperature(UNITS.RANKINE, 444.5, UNITS.CELSIUS, -30.9)).toEqual(RESULTS.INCORRECT);
+    expect(convertTemperature(UNITS.FAHRENHEIT, 6.5, UNITS.RANKINE, "cat")).toEqual(RESULTS.INCORRECT);
+    expect(convertTemperature("bird", "", UNITS.CELSIUS, 45.32)).toEqual(RESULTS.INVALID);
+})
